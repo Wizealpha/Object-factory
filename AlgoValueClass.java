1dfd9104877b498a6066d9e13df031bb0901d46a
Ndianagwa chiagozie <82313065+Wizealpha@users.noreply.github.com>
2023-05-13 10:47:44 +0100

    Update AlgoValueClass.java

--- AlgoValueClass.java
+++ AlgoValueClass.java
@@ -1,2 +1,10 @@
+import java.util.concurrent.ThreadLocalRandom;
 package PACKAGE_NAME;public class AlgoValueClass {
+  public class AlgoValueClass {
+    public static int AlgorValue = ThreadLocalRandom.current().nextInt(21, 23 + 1);
+    public static int PropertyValue1 = 87;
+    public static double PropertyValue2 = 45.87f;
+
+}
+
 }
