0cdcdc06c66a01b872ca802a008a075f33d7fd67
Ndianagwa chiagozie <82313065+Wizealpha@users.noreply.github.com>
2023-05-13 10:49:10 +0100

    Update CalcFactory.java

--- CalcFactory.java
+++ CalcFactory.java
@@ -1,2 +1,20 @@
 package PACKAGE_NAME;public class CalcFactory {
+   public static CalcSomething getCalculator() {
+        //open the configuration file
+        int alg = AlgoValueClass.AlgorValue;
+        int algoresult=alg % 5;
+
+        if (algoresult > 2 && algoresult < 4)
+            return new CalcUsingAlg3();
+        else if (algoresult < 2) {
+            float p1=AlgoValueClass.PropertyValue1;
+            double p2=AlgoValueClass.PropertyValue2;
+            return new CalcUsingAlg2(p1, p2);}
+         else { return new CalcUsingAl1();
+
+         }
+
+
+        }
+
 }
