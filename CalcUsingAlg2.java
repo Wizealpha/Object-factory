6da7f4beedcade002cb8400e69483fdbd3126063
Ndianagwa chiagozie <82313065+Wizealpha@users.noreply.github.com>
2023-05-13 10:52:11 +0100

    Update CalcUsingAlg2.java

--- CalcUsingAlg2.java
+++ CalcUsingAlg2.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class CalcUsingAlg2 {
+public class CalcUsingAlg2 implements CalcSomething{
+    private  float prop1;
+    private double prop2;
+
+    public  CalcUsingAlg2(float prop1, double prop2){
+        this.prop1 = prop1;
+        this.prop2 = prop2;
+    }
+
+
+    public double calculate(){
+        return  prop1 * prop2;
+    }
+    @Override
+    public String toString(){
+        return getClass().getName();
+    }
 }
