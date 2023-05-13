05a9181669c0ed7858d230fc24d3bea0f025d4d9
Ndianagwa chiagozie <82313065+Wizealpha@users.noreply.github.com>
2023-05-13 10:53:20 +0100

    Update CalcUsingAlg3.java

--- CalcUsingAlg3.java
+++ CalcUsingAlg3.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class CalcUsingAlg3 {
+public class CalcUsingAlg3 implements CalcSomething{
+    public  static double ap2 = 45.56;
+    public  static double ap3 = 102.98;
+    public static  float ap4 = 21.9f;
+
+
+    public double calculate(){
+
+        return ap2 * ap3 / ap4;
+
+    }
+    @Override
+    public String toString(){
+        return getClass().getName();
+    }
 }
